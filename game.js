d43037ba1b25a5c7952a52b74f2a8f3bacf8aa50
henryguo <1259425725@qq.com>
2023-03-02 20:49:48 -0800

    Instruction for operating

--- game.js
+++ game.js
@@ -156,6 +156,9 @@ export class Game extends Base_Scene {
 
     make_control_panel() {
         // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
+        this.control_panel.innerHTML += 'Use WASD to control Pacman 1, arrow keys to control Pacman 2.';
+        this.new_line();
+
         this.key_triggered_button("Change Colors", ["c"], this.set_colors);
         // Add a button for controlling the scene.
         this.key_triggered_button("Outline", ["o"], () => {
